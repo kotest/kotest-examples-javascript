6a8b3e4e91c21bece267375e1d05e9babfdaeac9
sksamuel <sam@sksamuel.com>
2021-04-25 13:51:08 -0500

    Updated dependencies

--- src/main/kotlin/ssn.kt
+++ src/main/kotlin/ssn.kt
@@ -5,7 +5,3 @@ private val socialRegex = RegExp("^\\d{3}-\\d{2}-\\d{4}$")
 fun validateSocial(ssn: String): Boolean {
    return socialRegex.test(ssn) && !ssn.contains("0") && !ssn.startsWith("666")
 }
-
-suspend fun animate() {
-
-}
