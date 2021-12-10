90bb50d750d388d0ea0b2661537d9e2f0c8a6f8e
sksamuel <sam@sksamuel.com>
2021-12-10 06:07:29 -0600

    Updated build to use kotest 5.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 }
 
 plugins {
-   kotlin("multiplatform").version("1.5.30")
-   id("io.kotest.multiplatform") version "5.0.0.5"
+   kotlin("multiplatform").version("1.6.0")
+   id("io.kotest.multiplatform") version "5.0.2"
 }
 
 repositories {
@@ -27,14 +27,14 @@ kotlin {
    sourceSets {
       val jsMain by getting {
          dependencies {
-             implementation("io.ktor:ktor-client-js:1.6.3")
+             implementation("io.ktor:ktor-client-js:1.6.7")
          }
       }
       val jsTest by getting {
          dependencies {
-            implementation("io.kotest:kotest-assertions-core-js:5.0.0.M1")
-            implementation("io.kotest:kotest-framework-engine-js:5.0.0.M1")
-            implementation("io.kotest:kotest-framework-datatest-js:5.0.0.M1")
+            implementation("io.kotest:kotest-assertions-core-js:5.0.1")
+            implementation("io.kotest:kotest-framework-engine-js:5.0.1")
+            implementation("io.kotest:kotest-framework-datatest-js:5.0.1")
          }
       }
    }
@@ -42,13 +42,13 @@ kotlin {
 
 tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().configureEach {
    kotlinOptions {
-      freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+      freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlin.RequiresOptIn"
    }
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
    kotlinOptions {
-      apiVersion = "1.5"
+      apiVersion = "1.6"
       verbose = true
    }
 }
